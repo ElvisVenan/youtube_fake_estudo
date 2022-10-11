b3a0b18676c8addc4e30d5041e58352ee9e5cc8d
Elvis <elvis@ezsolucoes.com.br>
2022-10-11 16:14:08 -0300

    Criado o modelo BottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/biblioteca.dart';
+import 'package:youtube/telas/emAlta.dart';
+import 'package:youtube/telas/inicio.dart';
+import 'package:youtube/telas/inscricao.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +12,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar:AppBar(
         iconTheme: IconThemeData(
@@ -45,7 +60,40 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container() ,
+
+      body: telas[_indiceAtual] ,
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        //type: BottomNavigationBarType.shifting,
+        type: BottomNavigationBarType.fixed,
+        selectedIconTheme: IconThemeData(color: Colors.grey),
+       fixedColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.red,
+            label: 'Início',
+              icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Início',
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            label: 'Início',
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            label: 'Início',
+            icon: Icon(Icons.folder),
+          ),
+        ],
+      ) ,
     );
   }
 }
